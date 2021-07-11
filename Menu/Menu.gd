d9b3d516259d020852bea3a3f7d60568bf3896fd
Raphael Pour <info@raphaelpour.de>
2021-07-11 18:19:17 +0200

    Menu: Add start animation

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -9,6 +9,8 @@ func _process(_delta):
 		$AudioStreamPlayer.play()
 	if (!$Background/AnimationPlayer.is_playing()):
 		$Background/AnimationPlayer.play("Move_City")
+	if (!$AnimationPlayer.is_playing()):
+		$AnimationPlayer.play("idle")
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://Levels/Level1/Level1.tscn")
