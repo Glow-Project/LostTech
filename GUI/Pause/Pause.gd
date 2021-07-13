a20413103ab17d6b8768ddbce1525146f8471e1a
Tch1b0 <Jpour3006@gmail.com>
2021-07-13 20:17:39 +0200

    Fixed 'Restart'

--- GUI/Pause/Pause.gd
+++ GUI/Pause/Pause.gd
@@ -10,3 +10,4 @@ func _on_Menu_pressed():
 
 func _on_Retry_pressed():
 	get_tree().change_scene("res://Levels/%s/%s.tscn" % [LevelSwitch.current_level, LevelSwitch.current_level])
+	Global.is_paused = false
